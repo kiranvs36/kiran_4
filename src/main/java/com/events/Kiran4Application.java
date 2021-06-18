f4e68105103ce8d57a56646c8e19e1c0eb5efdd4
Vijaykumar <kivijaykumar@deloitte.com>
2021-06-18 10:01:10 +0530

    initial setup

---
+++ src/main/java/com/events/Kiran4Application.java
@@ -0,0 +1,13 @@
+package com.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kiran4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kiran4Application.class, args);
+	}
+
+}
